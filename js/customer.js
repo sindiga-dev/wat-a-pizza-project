7bebca617d350fa762d26c34c8432635e4f6b8f8
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2023-03-26 11:02:16 +0300

    Adds validation

--- js/customer.js
+++ js/customer.js
@@ -1,13 +1,66 @@
-//Write  password validation code here 
-function setPasswordConfirmValidity() {
-    
-}
-
-
-// Write code to submit customer details 
 function submitCustomerDetail(event) {
-   
-}
-
-
-
+    event.preventDefault();
+  
+    // Get form values
+    const custID = document.getElementById("custID").value;
+    const name = document.getElementById("name").value;
+    const password = document.getElementById("custPasword").value;
+    const confirmPassword = document.getElementById("custConfirmPassword").value;
+    const email = document.getElementById("email").value;
+    const contact = document.getElementById("contact").value;
+    const address = document.getElementById("address").value;
+  
+    // Check if form values are valid
+    if (!custID || isNaN(custID)) {
+      alert("Please enter a valid customer ID");
+      return;
+    }
+  
+    if (!name) {
+      alert("Please enter a valid customer name");
+      return;
+    }
+  
+    if (!password) {
+      alert("Please enter a password");
+      return;
+    }
+  
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+  
+    if (!email || !validateEmail(email)) {
+      alert("Please enter a valid email address");
+      return;
+    }
+  
+    if (!contact || isNaN(contact)) {
+      alert("Please enter a valid contact number");
+      return;
+    }
+  
+    if (!address) {
+      alert("Please enter a valid address");
+      return;
+    }
+  
+    // Save customer details to json-server using Axios API
+    axios
+      .post("http://localhost:3000/customers", {
+        custID,
+        name,
+        password,
+        email,
+        contact,
+        address,
+      })
+      .then((response) => {
+        alert("Registration Successful");
+        document.getElementById("customerRegistrationForm").reset();
+      })
+      .catch((error) => {
+        alert("Error occurred while registering customer");
+      });
+  }
