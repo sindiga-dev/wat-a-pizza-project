a8dedbc452cff635272203b9a0fc236dd26ac184
adamswonder <adamswill.i.am71002@gmail.com>
2023-03-26 16:37:14 +0300

    adds post method

--- js/order.js
+++ js/order.js
@@ -2,4 +2,18 @@
 // in the previous sprint challenge
 
 //Save the order details captured from the form in json-server using Axios API
+axios({
+    method: "post",
+    url: `http://localhost:3001/order`,
+    data: bodyFormData,
+    headers: { "Content-Type": "multipart/form-data" },
+})
+    .then(function (response) {
+        //handle success
+        console.log(response);
+    })
+    .catch(function (response) {
+        //handle error
+        console.log(response);
+    });
 
