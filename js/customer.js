655aa5fc94fee88a1c45dc3b1c924708325fdb88
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2023-03-26 14:12:05 +0300

    Changes

--- js/customer.js
+++ js/customer.js
@@ -1,75 +1,76 @@
-import axios from 'axios';
+//  import axios from 'axios';
 
 
-function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  }
-  
-  function submitCustomerDetail(event) {
-    event.preventDefault();
-  
-    // Get form values
-    const custID = document.getElementById("custID").value;
-    const name = document.getElementById("name").value;
-    const password = document.getElementById("custPasword").value;
-    const confirmPassword = document.getElementById("custConfirmPassword").value;
-    const email = document.getElementById("email").value;
-    const contact = document.getElementById("contact").value;
-    const address = document.getElementById("address").value;
-  
-    // Check if form values are valid
-    if (!custID || isNaN(custID)) {
-      alert("Please enter a valid customer ID");
-      return;
+
+function  validateEmail ( email )   { 
+     const  emailRegex  =   /^[^\s@]+@[^\s@]+\.[^\s@]+$/ ; 
+     return  emailRegex . test ( email ) ; 
+   }
+
+function  submitCustomerDetail ( event )   {
+        event . preventDefault ( ) ; 
+    
+        // Get form values 
+        const  custID  =  document . getElementById ( "custID" ) . value ; 
+        const  name  =  document . getElementById ( "name" ) . value ; 
+        const  password  =  document . getElementById ( "custPasword" ) . value ; 
+        const  confirmPassword  =  document . getElementById ( "custConfirmPassword" ) . value ; 
+        const  email  =  document . getElementById ( "email" ) . value ; 
+        const  contact  =  document . getElementById ( "contact" ) . value ; 
+        const  address  =  document . getElementById ( "address" ) . value ; 
+    
+        // Check if form values are valid 
+        if   ( ! custID  ||  isNaN ( custID ) )   {
+        alert ( "Please enter a valid customer ID" ) ; 
+        return ; 
+        }
+    
+        if   ( ! name )   {
+        alert ( "Please enter a valid customer name" ) ; 
+        return ; 
+        }
+    
+        if   ( ! password )   {
+        alert ( "Please enter a password" ) ; 
+        return ; 
+        }
+    
+        if   ( password  !==  confirmPassword )   {
+        alert ( "Passwords do not match" ) ; 
+        return ; 
+        }
+    
+        if   ( ! email  ||   ! validateEmail ( email ) )   {
+        alert ( "Please enter a valid email address" ) ; 
+        return ; 
+        }
+    
+        if   ( ! contact  ||  isNaN ( contact ) )   {
+        alert ( "Please enter a valid contact number" ) ; 
+        return ; 
+        }
+    
+        if   ( ! address )   {
+        alert ( "Please enter a valid address" ) ; 
+        return ; 
+        }
+    
+        // Save customer details to json-server using Axios API 
+        axios
+        . post ( "http://localhost:3000/customer" ,   {
+            custID ,
+            name ,
+            password ,
+            email ,
+            contact ,
+            address ,
+        } )
+        . then ( ( response )   =>   {
+            alert ( "Registration Successful" ) ; 
+            document . getElementById ( "customerRegistrationForm" ) . reset ( ) ; 
+        } )
+        . catch ( ( error )   =>   {
+            alert ( "Error occurred while registering customer" ) ; 
+        }
+        ) ;
     }
-  
-    if (!name) {
-      alert("Please enter a valid customer name");
-      return;
-    }
-  
-    if (!password) {
-      alert("Please enter a password");
-      return;
-    }
-  
-    if (password !== confirmPassword) {
-      alert("Passwords do not match");
-      return;
-    }
-  
-    if (!email || !validateEmail(email)) {
-      alert("Please enter a valid email address");
-      return;
-    }
-  
-    if (!contact || isNaN(contact)) {
-      alert("Please enter a valid contact number");
-      return;
-    }
-  
-    if (!address) {
-      alert("Please enter a valid address");
-      return;
-    }
-  
-    // Save customer details to json-server using Axios API
-    axios
-      .post("http://localhost:3000/customer", {
-        custID,
-        name,
-        password,
-        email,
-        contact,
-        address,
-      })
-      .then((response) => {
-        alert("Registration Successful");
-        document.getElementById("customerRegistrationForm").reset();
-      })
-      .catch((error) => {
-        alert("Error occurred while registering customer");
-      });
-  }
-  
